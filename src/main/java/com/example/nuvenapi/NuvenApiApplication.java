5e1a9f51d896902db17c845d6aa720e785a2337c
NayraSousa <nayraamaral1@gmail.com>
2024-08-08 20:39:52 -0300

    first commit

---
+++ src/main/java/com/example/nuvenapi/NuvenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nuvenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvenApiApplication.class, args);
+	}
+
+}
