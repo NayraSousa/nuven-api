6abbef59cd7e66e3e2a1ad58d4207d8ea84d12ec
NayraSousa <nayraamaral1@gmail.com>
2024-08-10 10:19:29 -0300

    feat: create Adress model

---
+++ src/main/java/com/example/nuvenapi/model/Address.java
@@ -0,0 +1,32 @@
+package com.example.nuvenapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String district;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+
+}
