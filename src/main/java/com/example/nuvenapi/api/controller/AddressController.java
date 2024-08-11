040a342938e99a83ff61b1cf07176b138389d975
NayraSousa <nayraamaral1@gmail.com>
2024-08-11 11:26:51 -0300

    feature: implements address service

---
+++ src/main/java/com/example/nuvenapi/api/controller/AddressController.java
@@ -0,0 +1,26 @@
+package com.example.nuvenapi.api.controller;
+
+import com.example.nuvenapi.api.dto.AddressOutputDTO;
+import com.example.nuvenapi.service.AddressService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping("/read-all")
+    public List<AddressOutputDTO> readAll() {
+        return addressService.readAll();
+
+    }
+}
---
+++ src/main/java/com/example/nuvenapi/service/AddressService.java
@@ -0,0 +1,34 @@
+package com.example.nuvenapi.service;
+
+import com.example.nuvenapi.api.dto.AddressOutputDTO;
+import com.example.nuvenapi.api.dto.mapper.AddressMapper;
+import com.example.nuvenapi.model.Address;
+import com.example.nuvenapi.repository.AddressRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AddressService {
+
+    private final AddressRepository addressRepository;
+    private final AddressMapper addressMapper;
+
+    public AddressService(AddressRepository addressRepository, AddressMapper addressMapper) {
+        this.addressRepository = addressRepository;
+        this.addressMapper = addressMapper;
+    }
+
+    public List<AddressOutputDTO> readAll() {
+        List<Address> addresses = addressRepository.findAll();
+        List<AddressOutputDTO> addressesOutputDTOS = new ArrayList<>();
+        addressesOutputDTOS = addresses
+                .stream()
+                .map(addressMapper::toDTO)
+                .collect(Collectors.toList());
+        return addressesOutputDTOS;
+    }
+
+}
