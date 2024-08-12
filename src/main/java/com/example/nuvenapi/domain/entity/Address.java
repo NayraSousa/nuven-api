63f600c53a724fc9b0278519e148c810d10c1417
NayraSousa <nayraamaral1@gmail.com>
2024-08-12 10:12:34 -0300

    refactor: remove entity validations

--- src/main/java/com/example/nuvenapi/domain/entity/Address.java
+++ src/main/java/com/example/nuvenapi/domain/entity/Address.java
@@ -1,6 +1,7 @@
 package com.example.nuvenapi.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,23 +15,18 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotNull
     @Column(nullable = false)
     private String street;
 
-    @NotNull
     @Column(nullable = false)
     private String number;
 
-    @NotNull
     @Column(nullable = false)
     private String district;
 
-    @NotNull
     @Column(nullable = false)
     private String city;
 
-    @NotNull
     @Column(nullable = false)
     private String state;
 
