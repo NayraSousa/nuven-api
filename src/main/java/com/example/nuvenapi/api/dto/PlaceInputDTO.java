e9f41c492b3136c36f814c7de5a6ca0aa0cd1216
NayraSousa <nayraamaral1@gmail.com>
2024-08-12 10:12:34 -0300

    refactor: remove entity validations

--- src/main/java/com/example/nuvenapi/api/dto/PlaceInputDTO.java
+++ src/main/java/com/example/nuvenapi/api/dto/PlaceInputDTO.java
@@ -1,9 +1,6 @@
 package com.example.nuvenapi.api.dto;
 
-<<<<<<< HEAD
 import jakarta.validation.Valid;
-=======
->>>>>>> 3a446f0 (refactor: change dtos logic)
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
