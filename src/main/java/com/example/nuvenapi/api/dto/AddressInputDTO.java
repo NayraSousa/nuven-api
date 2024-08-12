bd7d614a087b4e711d07e6a6d46a610c82c4ef8d
NayraSousa <nayraamaral1@gmail.com>
2024-08-12 11:57:50 -0300

    refactor: add address field validation

--- src/main/java/com/example/nuvenapi/api/dto/AddressInputDTO.java
+++ src/main/java/com/example/nuvenapi/api/dto/AddressInputDTO.java
@@ -1,6 +1,7 @@
 package com.example.nuvenapi.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
@@ -11,13 +12,18 @@ public class AddressInputDTO {
     private UUID id;
 
     @NotBlank
+    @NotNull
     private String street;
+    @NotNull
     @NotBlank
     private String number;
+    @NotNull
     @NotBlank
     private String district;
     @NotBlank
+    @NotNull
     private String city;
+    @NotNull
     @NotBlank
     private String state;
 }
--- src/main/java/com/example/nuvenapi/api/dto/PlaceInputDTO.java
+++ src/main/java/com/example/nuvenapi/api/dto/PlaceInputDTO.java
@@ -1,5 +1,6 @@
 package com.example.nuvenapi.api.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class PlaceInputDTO {
     private String name;
 
     @NotNull
+    @Valid
     private AddressInputDTO address;
 
     @NotBlank
