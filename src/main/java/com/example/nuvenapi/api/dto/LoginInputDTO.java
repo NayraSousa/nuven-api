ebd1ae2742c89b98902d46faa039da3b0605f0e0
NayraSousa <nayraamaral1@gmail.com>
2024-08-12 19:37:45 -0300

    feat: create DTO login

---
+++ src/main/java/com/example/nuvenapi/api/dto/LoginInputDTO.java
@@ -0,0 +1,9 @@
+package com.example.nuvenapi.api.dto;
+
+import java.util.UUID;
+
+public class LoginInputDTO {
+
+    private String name;
+    private String password;
+}
---
+++ src/main/java/com/example/nuvenapi/api/dto/LoginOutputDTO.java
@@ -0,0 +1,6 @@
+package com.example.nuvenapi.api.dto;
+
+public class LoginOutputDTO {
+
+    private String token;
+}
